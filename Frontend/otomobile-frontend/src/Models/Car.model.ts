f2d94a24cf54e4c83cca07d17ca19e0d94bb24bd
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2019-05-03 03:39:47 +0300

    Car Model is written

---
+++ Frontend/otomobile-frontend/src/Models/Car.model.ts
@@ -0,0 +1,21 @@
+export class CarModel {
+    announcementDate: Date;
+    mark: String;
+    series: String;
+    model: String;
+    year: Number;
+    fuel: String;
+    gear: String;
+    km: Number;
+    chassisType: String;
+    enginePower: String;
+    engineDisplacement: String;
+    pull: String;
+    color: String;
+    guarantee: String;
+    plaque: String;
+    who: String;
+    swap: String;
+    state: String;
+    images: String;
+}
